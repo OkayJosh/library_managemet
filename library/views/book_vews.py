a40de396eddaeab87747b5a4f342fba3a4ec7c3a
Joshua Olatunji <joshua+github@etentlabs.com>
2024-09-12 13:50:12 +0100

    LM: views

---
+++ library/views/book_vews.py
@@ -0,0 +1,116 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from uuid import UUID
+from library.book_service import BookService
+
+
+class BookListView(APIView):
+    """
+    API view to list all available books.
+    """
+    def get(self, request):
+        book_service = BookService()
+        books = book_service.list_available_books()
+        return Response(books, status=status.HTTP_200_OK)
+
+
+class BorrowBookView(APIView):
+    """
+    API view to borrow a book.
+    """
+    def post(self, request):
+        user_uuid = UUID(request.data.get("user_uuid"))
+        book_uuid = UUID(request.data.get("book_uuid"))
+        days = request.data.get("days")
+
+        book_service = BookService()
+        try:
+            borrow_record = book_service.borrow_book(user_uuid, book_uuid, days)
+            return Response(borrow_record, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddBookView(APIView):
+    """
+    API view to add a new book.
+    """
+    def post(self, request):
+        book_data = request.data
+        book_service = BookService()
+
+        try:
+            new_book = book_service.add_new_book(book_data)
+            return Response(new_book, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RemoveBookView(APIView):
+    """
+    API view to add a new book.
+    """
+    def post(self, request, book_uuid):
+
+        book_uuid = UUID(book_uuid)
+        book_service = BookService()
+
+        try:
+            new_book = book_service.remove_book(book_uuid)
+            return Response(new_book, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BorrowedBookListView(APIView):
+    """
+    API view to list all borrowed books.
+    """
+    def get(self, request):
+        book_service = BookService()
+        borrowed_books = book_service.list_borrowed_books()
+        return Response(borrowed_books, status=status.HTTP_200_OK)
+
+
+class BookAvailabilityView(APIView):
+    """
+    API view to check book availability.
+    """
+    def get(self, request, book_uuid):
+        book_uuid = UUID(book_uuid)
+        book_service = BookService()
+        available = book_service.get_book_availability(book_uuid)
+        return Response({"available": available}, status=status.HTTP_200_OK)
+
+class BookDetailView(APIView):
+    """
+    API view to get a single book by its ID.
+    """
+    def get(self, request, book_uuid):
+        book_uuid = UUID(book_uuid)
+        book_service = BookService()
+        book = book_service.get_book_by_id(book_uuid)
+        return Response(book, status=status.HTTP_200_OK)
+
+
+class BookFilterView(APIView):
+    """
+    API view to filter books by publisher and category.
+    """
+    def get(self, request):
+        publisher = request.query_params.get("publisher")
+        category = request.query_params.get("category")
+        book_service = BookService()
+        filtered_books = book_service.filter_book(publisher, category)
+        return Response(filtered_books, status=status.HTTP_200_OK)
+
+class UnavailableBooksView(APIView):
+    """
+    API view to list books that are not available for borrowing.
+    """
+    def get(self, request):
+        book_service = BookService()
+        unavailable_books = book_service.list_unavailable_books()
+        return Response(unavailable_books, status=status.HTTP_200_OK)
---
+++ library/views/user_views.py
@@ -0,0 +1,54 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from uuid import UUID
+from library.user_service import UserService
+
+
+class UserListView(APIView):
+    """
+    API view to list all users.
+    """
+    def get(self, request):
+        user_service = UserService()
+        users = user_service.list_users()
+        return Response(users, status=status.HTTP_200_OK)
+
+
+class EnrollUserView(APIView):
+    """
+    API view to enroll a new user.
+    """
+    def post(self, request):
+        email = request.data.get("email")
+        firstname = request.data.get("firstname")
+        lastname = request.data.get("lastname")
+
+        user_service = UserService()
+        try:
+            new_user = user_service.enroll_user(email, firstname, lastname)
+            return Response(new_user, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserBorrowRecordsView(APIView):
+    """
+    API view to get a user's borrowed books.
+    """
+    def get(self, request, user_uuid):
+        user_uuid = UUID(user_uuid)
+        user_service = UserService()
+        borrow_records = user_service.get_user_borrow_records(user_uuid)
+        return Response(borrow_records, status=status.HTTP_200_OK)
+
+class UserBorrowedBooksView(APIView):
+    """
+    API view to list users and the books they have borrowed.
+    """
+    def get(self, request, user_uuid):
+        user_uuid = UUID(user_uuid)
+        user_service = UserService()
+        borrowed_books = user_service.get_user_borrow_records(user_uuid)
+        return Response(borrowed_books, status=status.HTTP_200_OK)
