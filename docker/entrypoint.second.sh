e317c49c8e944844aef6a8726a2fd144adec5f13
Joshua Olatunji <joshua+github@etentlabs.com>
2024-09-17 21:47:40 +0100

    LM: second entry point

---
+++ docker/entrypoint.second.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Navigate to the project directory
+PROJECT_DIR="$WORKDIR"
+echo "Changing to project directory: ${PROJECT_DIR}..."
+cd "${PROJECT_DIR}" || {
+    echo "Failed to change to project directory. Exiting." >&2
+    exit 1
+}
+
+# Log start of script
+echo "Starting Django application setup..."
+
+# Source the .env file and export environment variables
+echo "Loading environment variables from .env.second file..."
+if ! export $(grep -v '^#' .env.second | xargs); then
+    echo "Failed to export environment variables from .env file. Exiting." >&2
+    exit 1
+fi
+
+# Check if the database is reachable
+echo "Checking if the database is reachable..."
+if ! docker/reach_database.sh; then
+    echo "Database is not reachable. Exiting." >&2
+    exit 1
+fi
+
+# NOTE: i moved running migrations to the celery worker.
+# NOTE: why because this is where most of the actions occurs
+# Apply database migrations for multiple databases
+#DATABASES=("${POSTGRES_DB_1}" "${POSTGRES_DB_2}")
+#for DB in "${DATABASES[@]}"; do
+#    echo "Running database migrations for database: ${DB}..."
+#    if ! python manage.py migrate --database="${DB}"; then
+#        echo "Database migrations for ${DB} failed. Exiting." >&2
+#        exit 1
+#    fi
+#done
+
+# Start Gunicorn server
+echo "Starting Gunicorn server..."
+gunicorn library_managemet.wsgi:application \
+    --bind ":${GUNICORN_PORT:-8000}" \
+    --workers "${GUNICORN_WORKERS:-3}" \
+    --log-level "${GUNICORN_LOG_LEVEL:-info}" \
+    --access-logfile '-' \
+    --error-logfile '-' \
+    --timeout "${GUNICORN_TIMEOUT:-30}"
+
+# Log successful start
+echo "Gunicorn server started on port ${GUNICORN_PORT:-8000}."
